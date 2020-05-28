024d635faf4da82750f344f1574eba61c35344a6
ml0042993 <zhoujianxiong.1988@gmail.com>
2020-05-28 17:44:10 +0800

    添加新代理,准备更换代理逻辑

--- Core/Proxy/get_original_proxy.py
+++ Core/Proxy/get_original_proxy.py
@@ -1,39 +1,62 @@
+import aiohttp,asyncio
+import time
 from bs4 import BeautifulSoup
-from Core.Request_Site import Get_Request
 from Setting import Config
 from Setting.Init_Floder import Floder_Create
-class Original_Proxy(Get_Request):
+
+class Original_Proxy():
 	def __init__(self):
-		super().__init__()
-		self.SITE_URL = Config.Proxy_Site_Url_THREE
-		self.proxy = None
+		pass
+	async def get_Original_proxy(self,page):
+		conn = aiohttp.TCPConnector(ssl=False)
+		async with aiohttp.ClientSession(connector=conn) as session:
+			try:
+				async with session.get(page,timeout=10) as response:
 
-	def get_Original_proxy(self):
-		for title in Config.Proxy_Site_Url_THREE_pagename:
+					if response.status in Config.STATUS_CODES:
+						self.response = await response.text()
+						print(self.response)
+					else:
+						print("Error")
+			except Exception as e:
+				print(e)
+				print("error")
+				pass
 
-			self.page = Config.Proxy_Site_Url_THREE.format(title)
-			print(self.page)
-			self.proxy = None
-			self.get_noproxy_Html()
-			soup = BeautifulSoup(self.respones.text, "lxml")
+	async def get_Url_IP(self):
+		soup = BeautifulSoup(self.response, "lxml")
 
-			for allmessage in soup.select('tbody tr'):
-				# print(allmessage)
-				ip = allmessage.select('td a')[0].get_text()
-				port = allmessage.select('td')[1].get_text()
-				if allmessage.select('td')[4].get_text() == "不支持":
-					http = "http"
-				else:
-					http = "https"
-				proxy = "{}://{}:{}".format(http, ip, port)
+		for allmessage in soup.select('tbody tr'):
+			# print(allmessage)
+			ip = allmessage.select('td a')[0].get_text()
+			port = allmessage.select('td')[1].get_text()
+			if allmessage.select('td')[4].get_text() == "不支持":
+				http = "http"
+			else:
+				http = "https"
+			self.proxy = "{}://{}:{}".format(http, ip, port)
 
 	def run_main(self):
 
 		Floder_Create.create_Proxyfloder()
 		original_path = Floder_Create().Original_path#创建首次获取的代理存放的文件及文件位置
 		with open(original_path,'w') as f:
-					f.write(proxy)
-					f.write("\n")
+			try:
+				loop = asyncio.get_event_loop()
+				for i in range(0, len(Config.Proxy_Site_ihuan()), 10):
+					valid_Url = Config.Proxy_Site_ihuan()[i:i + 10]
+					# print(test_proxy)
+					# for url in valid_Url:
+					# 	task = self.get_Original_proxy(url)
+					# 	tasks.append(task)
+					tasks = [self.get_Original_proxy(Url) for Url in valid_Url]
+					loop.run_until_complete(asyncio.wait(tasks))
+					f.write(self.proxy)
+					time.sleep(5)
+			except Exception as e:
+				print('xxxx')
+
+
 if __name__ == '__main__':
 	obj = Original_Proxy()
 	obj.run_main()
--- Setting/Config.py
+++ Setting/Config.py
@@ -6,8 +6,8 @@ China_Url = 'https://www.98asde.info/forum-2-1.html'
 Proxy_Site_Url_ONE = "https://www.xicidaili.com/nn/"  # 代理网站地址
 # Proxy_Site_Url_ONE = "https://www.xicidaili.com/nt/"  # 代理网站地址
 Proxy_Site_Url_TOW = "https://ip.jiangxianli.com/?page={page}&anonymity=2"  # 代理网站地址
-Proxy_Site_Url_THREE = 'https://ip.ihuan.me/anonymity/2.html?page={}'
-Proxy_Site_Url_THREE_pagename = ["b97827cc","4ce63706","5crfe930","f3k1d581","ce1d45977","881aaf7b5","eas7a436","981o917f5","2d28bd81a","a42g5985d","came0299","e92k59727",
+Proxy_Site_Url_ihuan = 'https://ip.ihuan.me/anonymity/2.html?page={}'
+Proxy_Site_Url_ihuan_pagename = ["b97827cc","4ce63706","5crfe930","f3k1d581","ce1d45977","881aaf7b5","eas7a436","981o917f5","2d28bd81a","a42g5985d","came0299","e92k59727",
 		 "242r0e7b5","bc265a560","622b6a5d3","ae3g7e7aa","b01j07395","68141a2df","904545743","0134c4568"]
 #请求头
 HEADERS = {
@@ -15,3 +15,14 @@ HEADERS = {
 		}
 
 STATUS_CODES = [200]
+
+def Proxy_Site_ihuan():
+	valid_Url=[]
+	for pageend in Proxy_Site_Url_ihuan_pagename:
+		Url = Proxy_Site_Url_ihuan.format(pageend)
+		valid_Url.append(Url)
+	return valid_Url
+
+if __name__ == '__main__':
+	for i in Proxy_Site_ihuan():
+		print(i)
